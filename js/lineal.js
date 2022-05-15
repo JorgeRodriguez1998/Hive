e0515187f7683b43a4e4bae78a02a66b4cfb64ce
JorgeRodriguez1998 <jorgerodriguez5tos@gmail.com>
2022-05-15 08:35:16 -0500

    Adding Charts

---
+++ js/lineal.js
@@ -0,0 +1,43 @@
+// Obtener una referencia al elemento canvas del DOM
+const $grafica = document.querySelector("#grafica1");
+// Las etiquetas son las que van en el eje X. 
+const etiquetas = ["Enero", "Febrero", "Marzo", "Abril"]
+// Podemos tener varios conjuntos de datos. Comencemos con uno
+const datosPreventivos2022 = {
+    label: "Mantenimientos Preventivos",
+    data: [66, 67, 64, 60], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
+    borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
+    borderWidth: 1,// Ancho del borde
+};
+
+const datosCorrectivos2022 = {
+    label: "Mantenimientos correctivos",
+    data: [20, 12, 22, 23], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    backgroundColor: 'rgba(255, 159, 64, 0.2)',// Color de fondo
+    borderColor: 'rgba(255, 159, 64, 1)',// Color del borde
+    borderWidth: 1,// Ancho del borde
+};
+
+new Chart($grafica, {
+    type: 'line',// Tipo de gráfica
+    data: {
+        labels: etiquetas,
+        datasets: [
+            datosCorrectivos2022,
+            datosPreventivos2022,
+            // Aquí más datos...
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+        },
+    }
+});
+
+
