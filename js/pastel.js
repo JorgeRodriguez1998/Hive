6f1f349c444bbd035d7409a770181d5d43a3313b
JorgeRodriguez1998 <jorgerodriguez5tos@gmail.com>
2022-05-15 10:02:00 -0500

    Updating charts

--- js/pastel.js
+++ js/pastel.js
@@ -7,10 +7,10 @@ const datosIngresos = {
     data: [15, 10, 35, 10, 30 ], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
     // Ahora debería haber tantos background colors como datos, es decir, para este ejemplo, 4
     backgroundColor: [
-        'rgba(163,221,203,0.2)',
-        'rgba(232,233,161,0.2)',
-        'rgba(230,181,102,0.2)',
-        'rgba(229,112,126,0.2)',
+        'rgba(163,221,203)',
+        'rgba(232,233,161)',
+        'rgba(230,181,102)',
+        'rgba(229,112,126)',
     ],// Color de fondo
     borderColor: [
         'rgba(163,221,203,1)',
